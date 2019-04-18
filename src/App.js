4a35272ceaf85164d92a6bf239bf2af51ba377a8
André Sebastian <a.sebastian@posteo.de>
2019-04-18 18:01:23 +0200

    add post form and post to api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Posts from './components/Posts'
+import Postform from './components/Postform'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Postform/>
+        <hr />
         <Posts />
       </div>
     );
--- src/components/Postform.jsx
+++ src/components/Postform.jsx
@@ -1,11 +1,63 @@
 import React, { Component } from 'react'
 
-export default class Postform extends Component {
+class Postform extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      title: '',
+      body: ''
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const post = {
+      title: this.state.title,
+      body: this.state.body
+    }
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(post)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+
   render() {
+
     return (
       <div>
-        
+        <h1>Add Post</h1>
+        <form onSubmit={this.onSubmit}>
+          <div>
+            <label htmlFor="title">Title</label><br />
+            <input name="title" type="text" onChange={this.onChange} value={this.state.title}/>
+          </div>
+          <br />
+          <div>
+            <label htmlFor="">Body</label><br />
+            <textarea name="body" id="" cols="30" rows="10" onChange={this.onChange} value={this.state.body}></textarea>
+          </div>
+          <br />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     )
   }
 }
+
+export default Postform;
