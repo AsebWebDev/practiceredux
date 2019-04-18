ce1326e1e22595b1d837ea14273d4e937f5a5616
André Sebastian <a.sebastian@posteo.de>
2019-04-18 19:15:31 +0200

    add store and intital reducer, add redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import  { Provider } from 'react-redux'
 import Posts from './components/Posts'
 import Postform from './components/Postform'
+import store from './store'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Postform/>
-        <hr />
-        <Posts />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <Postform/>
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
